c15247ee4563ee48882ae550da22a5ba0b942f4e
Ognjen Blazic <ognjen.blazic.dev@gmail.com>
2020-04-13 15:03:13 +0200

    Binary tree - operations and tests

---
+++ data_structures/binary_tree.py
@@ -0,0 +1,44 @@
+class BinaryTree:
+    head = None
+    count = 0
+
+    def add(self, node):
+        if self.head is None:
+            self.head = node
+            self.count = 1
+            return
+
+        self.link(self.head, node)
+        self.count = self.count + 1
+
+    def link(self, current_head, node):
+        if node.value < current_head.value:
+            if current_head.left is None:
+                current_head.left = node
+            else:
+                self.link(current_head.left, node)
+        else:  # equals values are considered as "greater"
+            if current_head.right is None:
+                current_head.right = node
+            else:
+                self.link(current_head.right, node)
+
+    def contains(self, value):
+        current = self.head
+
+        while current is not None:
+            if current.value == value:
+                return True
+
+            if value < current.value:
+                current = current.left
+            else:
+                current = current.right
+        return False
+
+    def in_order_traversal(self, node):
+        if node is None:
+            return
+        self.in_order_traversal(node.left)
+        print(node.value)
+        self.in_order_traversal(node.right)
---
+++ data_structures/binary_tree_node.py
@@ -0,0 +1,7 @@
+class BinaryTreeNode:
+    value = None
+    left = None
+    right = None
+
+    def __init__(self, value):
+        self.value = value
